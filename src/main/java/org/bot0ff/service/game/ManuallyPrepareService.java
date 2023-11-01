a551afe6ef67a3b95959bf12f65702354281672f
Bot0FF <92510704+Bot0FF@users.noreply.github.com>
2023-11-01 10:29:27 +0300

    Telegram bot "SeaBattle"

--- src/main/java/org/bot0ff/service/game/ManuallyPrepareService.java
+++ src/main/java/org/bot0ff/service/game/ManuallyPrepareService.java
@@ -161,77 +161,27 @@ public class ManuallyPrepareService {
         //если нажатие по однопалубному кораблю
         if(userFiledArr[ver][hor] == 1) {
             userFiledArr[ver][hor] = 0;
-//            if (ver > 1 && userFiledArr[(ver - 2)][hor] == 0) {
-//                userFiledArr[(ver - 1)][hor] = 0; //клетка над кораблем
-//            }
-//            if (ver < GAME_FILED_LENGTH - 2 && userFiledArr[(ver + 2)][hor] == 0) {
-//                userFiledArr[(ver + 1)][hor] = 0; //клетка под кораблем
-//            }
-//            if (hor > 1 && userFiledArr[ver][hor - 2] == 0) {
-//                userFiledArr[(ver)][hor - 1] = 0; //клетка слева от корабля
-//            }
-//            if (hor < GAME_FILED_LENGTH - 2 && userFiledArr[ver][hor + 2] == 0) {
-//                userFiledArr[(ver)][hor + 1] = 0; //клетка справа от корабля
-//            }
-
         }
 
-//        //если нажатие по двухпалубному кораблю
-//        else if(userFiledArr[ver][hor] == 2) {
-//            userFiledArr[ver][hor] = 5;
-//            //меняем клетки вокруг корабля
-//            if (ver > 0 && userFiledArr[(ver - 1)][hor] == 2) {
-//                userFiledArr[(ver - 1)][hor] = 1; //клетка над кораблем
-//                userFiledArr[(ver - 1)][hor - 1] = 5;
-//                userFiledArr[(ver - 1)][hor + 1] = 5;
-//                if(ver < GAME_FILED_LENGTH - 1) {
-//                    userFiledArr[ver + 1][hor] = 0;
-//                    if(userFiledArr[ver + 1][hor - 2] == 0) {
-//                        userFiledArr[ver + 1][hor - 1] = 0;
-//                    }
-//                    if(userFiledArr[ver + 1][hor + 2] == 0) {
-//                        userFiledArr[ver + 1][hor + 1] = 0;
-//                    }
-//                }
-//            }
-//            if (ver < GAME_FILED_LENGTH - 1 && userFiledArr[ver + 1][hor] == 2) {
-//                userFiledArr[(ver + 1)][hor] = 1; //клетка под кораблем
-//                if(ver > 0) {
-//                    userFiledArr[(ver - 1)][hor] = 0;
-//                    userFiledArr[(ver - 1)][hor - 1] = 0;
-//                    userFiledArr[(ver - 1)][hor + 1] = 0;
-//                }
-//            }
-//            if (hor > 0 && userFiledArr[ver][hor - 1] == 2) {
-//                userFiledArr[(ver)][hor - 1] = 1; //клетка слева от корабля
-//                if(hor < GAME_FILED_LENGTH - 1) {
-//                    userFiledArr[ver][hor - 1] = 0;
-//                    userFiledArr[ver - 1][hor - 1] = 0;
-//                    userFiledArr[ver + 1][hor - 1] = 0;
-//                }
-//            }
-//            if (hor < GAME_FILED_LENGTH - 1 && userFiledArr[ver][hor + 1] == 2) {
-//                userFiledArr[ver][hor + 1] = 0; //клетка справа от корабля
-//                if(hor > 0) {
-//                    userFiledArr[ver][hor + 1] = 0;
-//                    userFiledArr[ver - 1][hor + 1] = 0;
-//                    userFiledArr[ver + 1][hor + 1] = 0;
-//                }
-//            }
-//
-//            //меняем клетки по углам корабля
-//            if (ver > 0 & hor > 0) {
-//                userFiledArr[(ver - 1)][(hor - 1)] = 0; //клетка слева сверху
-//            }
-//            if (ver > 0 & hor < (GAME_FILED_LENGTH - 1)) {
-//                userFiledArr[(ver - 1)][(hor + 1)] = 0; //клетка справа сверху
-//            }
-//            if (ver < (GAME_FILED_LENGTH - 1) & hor > 0) {
-//                userFiledArr[(ver + 1)][(hor - 1)] = 0; //клетка слева снизу
-//            }
-//            if (ver < (GAME_FILED_LENGTH - 1) & hor < (GAME_FILED_LENGTH - 1)) {
-//                userFiledArr[(ver + 1)][(hor + 1)] = 0; //клетка справа снизу
-//            }
+        //если нажатие по двухпалубному кораблю
+        else if(userFiledArr[ver][hor] == 2) {
+            userFiledArr[ver][hor] = 5;
+            //меняем клетки вокруг корабля
+            if (ver > 0 && userFiledArr[(ver - 1)][hor] == 2) {
+                userFiledArr[(ver - 1)][hor] = 1; //клетка над кораблем
+            }
+            else if (ver < GAME_FILED_LENGTH - 1 && userFiledArr[ver + 1][hor] == 2) {
+                userFiledArr[(ver + 1)][hor] = 1; //клетка под кораблем
+            }
+            else if (hor > 0 && userFiledArr[ver][hor - 1] == 2) {
+                userFiledArr[ver][hor - 1] = 1; //клетка под кораблем
+            }
+            else if (hor < GAME_FILED_LENGTH - 1 && userFiledArr[ver][hor + 1] == 2) {
+                userFiledArr[ver][hor + 1] = 1; //клетка под кораблем
+            }
+        }
+
+        //удаляет клетки 5 и 6, рядом с которыми нет кораблей
         for(int v = 0; v < GAME_FILED_LENGTH; v++) {
             for(int h = 0; h < GAME_FILED_LENGTH; h++) {
                 if(userFiledArr[v][h] == 5 | userFiledArr[v][h] == 6) {
